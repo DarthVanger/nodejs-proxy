4b8341497570db7d035cfdf98a07a6e4431dbbd7
DarthVanger <darthvanger@gmail.com>
2016-05-31 16:56:10 -0400

    Added port forwarding to some sites; Commented out reading hotstname, because it caused server to crash.

--- proxy.js
+++ proxy.js
@@ -4,23 +4,38 @@ var http = require('http'),
     url = require('url');
 
 http.createServer(function(req, res) {
-    var hostname = req.headers.host.split(":")[0];
+    //var hostname = req.headers.host.split(":")[0];
     var pathname = url.parse(req.url).pathname;
 
-    console.log('hostname = ' + hostname);
-    console.log('pathname = ' + pathname);
+    //console.log('hostname = ' + hostname + '\n');
+    //console.log('pathname = ' + pathname + '\n');
 
-    switch(pathname)
-    {
-        case '/futon-art':
-            proxy.web(req, res, { target: 'http://localhost:8000' });
-            break;
-        case '/jsonFileDb':
-            proxy.web(req, res, { target: 'http://localhost:9000' });
-            break;
-        default:
-            proxy.web(req, res, { target: 'http://localhost:8000' });
+    // port 8000 is apache
+    var sites = {
+        '/jsonFileDb': 'http://localhost:9000',
+        '/cash-machine': 'http://localhost:9001',
+        '/futon-art': 'http://localhost:8000',
+        '/electronics-shop': 'http://localhost:9002',
+        '/': 'http://localhost:8000'
     }
+
+    var noSiteWasMatched = true;
+    for (var site in sites) {
+        var rootRegex = new RegExp(site + '/?');
+        var sitePageRegex = new RegExp(site + '/.*');
+        if (pathname.match(rootRegex) || pathname.match(sitePageRegex)) {
+	    //console.log('matched ' + site);
+            noSiteWasMatched = false;
+            proxy.web(req, res, { target: sites[site] });
+            break;
+        }
+    }
+
+    if (noSiteWasMatched) {
+        res.statusCode = 404;
+        res.end('Proxy server: No subsite was found for url "' + req.url + '"');
+    }
+
 }).listen(80, function() {
     console.log('proxy listening on port 80');
 });
